fcbcbb4ee9ac9ae51927f1a944847a073a6112fb
poinka <pkorobejnikova18@gmail.com>
2025-06-29 23:04:59 +0300

    added search in database and changed selected quotes

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,13 +3,11 @@ import torch
 import faiss
 import numpy as np
 import pandas as pd
-import torch
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct, VectorParams, Distance
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import numpy as np
 import uuid
-
+import random
 
 
 class VectorDB:
@@ -17,12 +15,16 @@ class VectorDB:
         print("Init database")
         self.dim = dim
         self.collection_name = collection_name
+        self.nbits = 128
         self.model = AutoModelForSequenceClassification.from_pretrained('SamLowe/roberta-base-go_emotions')
         self.tokenizer = AutoTokenizer.from_pretrained('SamLowe/roberta-base-go_emotions')
         self.client = QdrantClient(":memory:")  # или url="http://localhost:6333"
+        self.index = faiss.IndexLSH(self.dim, self.nbits)
+        self.vector_ids = []
         self.__setup_db()
 
     def __setup_db(self):
+        print("Setup database")
         # создаём коллекцию, если не существует
         if not self.client.collection_exists(self.collection_name):
             self.client.create_collection(
@@ -42,10 +44,18 @@ class VectorDB:
         print("Fill database")
         quotes = pd.read_csv("selected_quotes_embeddings.csv")
         limit = 100000
+        vectors = []
         for i, row in quotes.iterrows():
-            self.add_text(row['quote'], row['category'], row['embeddings'])
+            if isinstance(row['embeddings'], str):
+                vector = np.array(eval(row['embeddings']), dtype=np.float32)
+            else:
+                vector = row['embeddings']
+            vector = vector / np.linalg.norm(vector)
+            vectors.append(vector)
+            self.add_text(row['quote'], row['category'], vector)
             if i > limit:
                 break
+        self.index.add(np.array(vectors, dtype=np.float32))
 
     def encode(self, text, device='cpu'):
         # Tokenization
@@ -62,29 +72,35 @@ class VectorDB:
         cls_embedding = hidden_states[:, 0, :].squeeze().cpu().numpy()
         return cls_embedding.astype(np.float32)
 
-    def add_text(self, text: str, category: str, embeddings: str):
+    def add_text(self, text: str, category: str, vector):
         #vector = self.encode(text)
-        if isinstance(embeddings, str):
-            vector = np.array(eval(embeddings), dtype=np.float32)
-        else:
-            vector = embeddings
+        if isinstance(vector, str):
+            vector = np.array(eval(vector), dtype=np.float32)
+        
         payload = {"text": text, "category": category}
+        vector_id = str(uuid.uuid4())
         point = PointStruct(
-            id=str(uuid.uuid4()),
+            id=vector_id,
             vector=vector.tolist(),
             payload=payload
         )
         self.client.upsert(collection_name=self.collection_name, points=[point])
+        self.vector_ids.append(vector_id)
+        self.index.add(vector.reshape(1, -1))
 
-    def search(self, text: str, top_k: int = 1):
+    def search(self, text: str, top_k: int = 10):
         vector = self.encode(text)
-        results = self.client.query_points(
-            collection_name=self.collection_name,
-            query=vector.tolist(),
-            limit=top_k,
-            with_payload=True
-        )
-        return [hit.payload['text'] for hit in results.points][0]
+        vector = vector / np.linalg.norm(vector)
+        vector = vector.reshape(1, -1).astype(np.float32)
+        _, indices = self.index.search(vector, top_k)
+        if len(indices[0]) == 0:
+            return "No matches found"
+        nearest_ids = [self.vector_ids[idx] for idx in indices[0] if idx < len(self.vector_ids)]
+        if not nearest_ids:
+            return "No matches found"
+        selected_id = random.choice(nearest_ids)
+        result = self.client.retrieve(collection_name=self.collection_name, ids=[selected_id], with_payload=True)
+        return result[0].payload["text"]
 
 
 if __name__ == '__main__':
